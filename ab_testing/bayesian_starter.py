f85cb6eb472ea8c459f0d7ef637e738d1b0554f0
CHUQING SHI <chuqing@Brendas-Mac-mini.local>
2025-03-11 22:41:01 -0700

    Added Exercises 53&54: Baysian Bandits/Thompson Sampling

--- ab_testing/bayesian_starter.py
+++ ab_testing/bayesian_starter.py
@@ -20,19 +20,19 @@ BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
 class Bandit:
   def __init__(self, p):
     self.p = p
-    self.a = # TODO
-    self.b = # TODO
+    self.a = 1 # TODO
+    self.b = 1 # TODO
     self.N = 0 # for information only
 
   def pull(self):
     return np.random.random() < self.p
 
   def sample(self):
-    return # TODO - draw a sample from Beta(a, b)
+    return beta.rvs(self.a, self.b) # TODO - draw a sample from Beta(a, b)
 
   def update(self, x):
-    self.a = # TODO
-    self.b = # TODO
+    self.a = self.a + x # TODO
+    self.b = self.b + 1 - x # TODO
     self.N += 1
 
 
@@ -53,7 +53,7 @@ def experiment():
   rewards = np.zeros(NUM_TRIALS)
   for i in range(NUM_TRIALS):
     # Thompson sampling
-    j = # TODO
+    j = np.argmax([b.sample() for b in bandits]) # TODO
 
     # plot the posteriors
     if i in sample_points:
@@ -68,8 +68,7 @@ def experiment():
     # update the distribution for the bandit whose arm we just pulled
     bandits[j].update(x)
 
-  # print total reward
-  print("total reward earned:", rewards.sum())
+  # print total reward print("total reward earned:", rewards.sum())
   print("overall win rate:", rewards.sum() / NUM_TRIALS)
   print("num times selected each bandit:", [b.N for b in bandits])
 
